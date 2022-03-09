36343c2170250a2dfa2acf6f29e8afedd452c6c1
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-03-09 14:35:35 +0600

    Backend. Create models and config file.

---
+++ backend/models/Post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const PostSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    user: {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    date: new Date().toISOString(),
+    description: null | String,
+    image: null | String,
+});
+
+const Post = mongoose.model('Post', PostSchema);
+
+module.exports = Post;
