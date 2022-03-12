811f7a812e4c15d86e35f93a62627555e8e78e57
Asylbek Kydraliev <asyl.kydraliev@gmail.com>
2022-03-12 09:41:18 +0600

    Fixtures data.

---
+++ backend/fixtures.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const config = require('./config');
+const User = require("./models/User");
+const Post = require("./models/Post");
+const Comment = require("./models/Comment");
+
+const run = async () => {
+    await mongoose.connect(config.mongo.db, config.mongo.options);
+
+    const collections = await mongoose.connection.db.listCollections().toArray();
+
+    for (const coll of collections) {
+        await mongoose.connection.db.dropCollection(coll.name);
+    }
+
+    const [user, user1] = await User.create({
+        email: 'user@gmail.com',
+        name: 'user',
+        password: '123',
+        token: '5enDI2paOqusPavVWOnwB'
+    }, {
+        email: 'user1@gmail.com',
+        displayName: 'user1',
+        password: '123',
+        token: '8enDI2paOqusBavVWOnwL'
+    });
+
+    const [post1, post2] =  await Post.create({
+        user: user,
+        date: '01.01.2022',
+        title: 'New post',
+        description: 'New post description',
+    }, {
+        user: user1,
+        date: '01.01.2022',
+        title: 'New post1',
+        description: 'New post description1',
+    });
+
+    await Comment.create({
+        user: user,
+        post: post2,
+        text: 'New comment'
+    }, {
+        user: user1,
+        post: post1,
+        text: 'New comment1'
+    });
+
+    await mongoose.connection.close();
+};
+
+run().catch(e => console.log(e));
